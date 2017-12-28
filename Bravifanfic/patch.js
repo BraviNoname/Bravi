62b2987131250eb8ef2934056d1abc11cf82f139
nmlgc <nmlgc@nmlgc.net>
2017-12-28 03:51:46 +0100

    Add thcrap repository information.

--- Bravifanfic/patch.js
+++ Bravifanfic/patch.js
@@ -1,3 +1,11 @@
 {
-  "fontrules": {"*": "'RussellSquare' 32 11 100"}
+	"fontrules": {
+		"*": "'RussellSquare' 32 11 100"
+	},
+	"id": "Bravifanfic",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/Bravi/Bravifanfic/",
+		"http://mirrors.thpatch.net/Bravi/Bravifanfic/"
+	],
+	"title": "Some (incomplete) fanfic mod with some retext as well."
 }
---
+++ repo.js
@@ -0,0 +1,12 @@
+{
+	"contact": "BraviNoname#9568",
+	"id": "Bravi",
+	"patches": {
+		"Bravifanfic": "Some (incomplete) fanfic mod with some retext as well."
+	},
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/Bravi/",
+		"http://mirrors.thpatch.net/Bravi/"
+	],
+	"title": "Bravi's patches"
+}
