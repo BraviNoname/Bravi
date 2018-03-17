55eaae5b1dd287e74bfa3692eadc2068afbc9871
BraviNoname <34876478+BraviNoname@users.noreply.github.com>
2018-03-17 12:01:48 +0100

    Add files via upload

--- repo.js
+++ repo.js
@@ -2,11 +2,11 @@
 	"contact": "BraviNoname#9568",
 	"id": "Bravi",
 	"patches": {
-		"Bravifanfic": "Some (incomplete) fanfic mod with some retext as well."
+		"Bravifanfic": "Bravi's Story That Never Was\n                           Some (incomplete) fanfic mod with some retext as well.\n                          (see the readme.txt files for BGM because they are yet to be changed, ask me for the bgm.dat files once this is done)\n                          (dialogues are still yet to be done too)"
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/Bravi/",
 		"http://mirrors.thpatch.net/Bravi/"
 	],
-	"title": "Bravi's Strange Story and Mod That Never Was"
+	"title": "Bravi's Strange Creativity"
 }
