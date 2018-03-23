58f9cf3444584063c09106e30e33cbf01d04d66b
BraviNoname <34876478+BraviNoname@users.noreply.github.com>
2018-03-23 09:58:57 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "BraviNoname#9568",
 	"id": "Bravi",
 	"patches": {
-		"Bravifanfic": "Bravi's Story That Never Was\n                           Some (incomplete) fanfic mod with some retext as well.\n                          (open the readme.md files (with Notepad) for BGM because they are yet to be changed, ask me for the bgm.dat files once this is done)\n                          (dialogues are still yet to be done too)",
+		"Bravifanfic": "Bravi's Story That Never Was\n                           (open the readme.md files (with Notepad) for important details)",
 		"Bravimemes": "Modding the game for whatever reason"
 	},
 	"servers": [
