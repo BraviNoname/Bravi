83ae92af2a271fdd75c445b830dac18e4196b7b3
BraviNoname <34876478+BraviNoname@users.noreply.github.com>
2018-03-23 09:56:44 +0100

    Update patch.js

--- Bravimemes/patch.js
+++ Bravimemes/patch.js
@@ -13,5 +13,5 @@
 		"http://thcrap.nmlgc.net/repos/Bravi/Bravimemes/",
 		"http://mirrors.thpatch.net/Bravi/Bravimemes/"
 	],
-	"title": "Modding the game just to make it look like something else"
+	"title": "Modding the game for whatever reason"
 }
