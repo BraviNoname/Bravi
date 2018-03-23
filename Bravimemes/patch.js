e724bea4fe4980f8a6b1727c44b7e05f9efb549b
BraviNoname <34876478+BraviNoname@users.noreply.github.com>
2018-03-23 09:44:01 +0100

    Like this Nmlgc ?

---
+++ Bravimemes/patch.js
@@ -0,0 +1,16 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"fontrules": {
+		"*": "'Power Clear'"
+	},
+	"id": "Bravifanfic",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/Bravi/Bravimemes/",
+		"http://mirrors.thpatch.net/Bravi/Bravimemes/"
+	],
+	"title": "Modding the game just to make it look like something else"
+}
--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "BraviNoname#9568",
 	"id": "Bravi",
 	"patches": {
-		"Bravifanfic": "Bravi's Story That Never Was\n                           Some (incomplete) fanfic mod with some retext as well.\n                          (open the readme.md files (with Notepad) for BGM because they are yet to be changed, ask me for the bgm.dat files once this is done)\n                          (dialogues are still yet to be done too)"
+		"Bravifanfic": "Bravi's Story That Never Was\n                           Some (incomplete) fanfic mod with some retext as well.\n                          (open the readme.md files (with Notepad) for BGM because they are yet to be changed, ask me for the bgm.dat files once this is done)\n                          (dialogues are still yet to be done too)",
+		"Bravimemes": "Modding the game just to make it look like something else"
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/Bravi/",
