371dec36f9dc7e8d684617ac1c79a7831707f581
BraviNoname <34876478+BraviNoname@users.noreply.github.com>
2018-07-08 16:18:36 +0200

    Add files via upload

---
+++ Braviretex/README.md
@@ -0,0 +1,3 @@
+That's just the same as the Bravifanfic mod, but without the fanfic.
+
+I recommend to put Gastari's touhou 7 retex under this patch for some backgrounds though.
---
+++ Braviretex/patch.js
@@ -0,0 +1,17 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en"
+	],
+	"fontrules": {
+		"*": "'Power Clear'"
+	},
+	"id": "Bravifanfic",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/Bravi/Braviretex/",
+		"http://mirrors.thpatch.net/Bravi/Braviretex/"
+	],
+	"title": "Bravi's Retex But No Fanfic"
+}
