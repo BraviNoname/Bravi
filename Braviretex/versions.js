c93fe27002c6fada198d960d8da1f45c0f186b38
BraviNoname <34876478+BraviNoname@users.noreply.github.com>
2018-08-27 02:17:33 +0200

    compatibility for the retex patch

--- Braviretex/versions.js
+++ Braviretex/versions.js
@@ -1,13 +1,13 @@
 {
     "hashes": {
-		"bbe545900af68fef0dd253e9afa51d828f08937656bd7af74cc33dda03cf09b7": ["th06", "v1.00b", "Practice patch by ACK"],
+		"7627b6dd5104d4efc249e6c3666eafffa97cf0194a12ddd52f575be27b073548": ["th06", "v1.02h", "Practice patch by ACK"],
 		"fc915bd89fd091f2a74514d714d654215069cfaf07ac3b27ebf73be1a2fb0627": ["th07", "v1.00b", "Practice patch by ACK"],
 		"e46945c0cd68a8cd4e92f7decb981adc7e80cf1df1139caa0800fa886c955a32": ["th10", "v1.00a", "Practice patch by ACK"],
 		"a995da8a1aa0133d5c425174713c02f255a366bd56ccf90221cfd9524cbd74e1": ["th12", "v1.00b", "Practice patch by ACK"],
         "cf8d15b427248e1bcfe5e55cc4e17227dba2f15f1a7f8b2285cb6a263464fd4e": ["th15", "v1.00b", "Practice patch by ACK"]
     },
     "sizes": {
-		"544768": ["th06", "v1.00b", "Practice patch by ACK"],
+		"544768": ["th06", "v1.02h", "Practice patch by ACK"],
 		"683520": ["th07", "v1.00b", "Practice patch by ACK"],
 		"504320": ["th10", "v1.00a", "Practice patch by ACK"],
 		"770048": ["th12", "v1.00b", "Practice patch by ACK"],
