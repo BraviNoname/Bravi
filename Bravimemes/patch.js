a59f1dee756ee2dad596374652d0ee82042f265a
BraviNoname <braviaggron@gmail.com>
2019-02-24 07:09:51 +0100

    removing the font

--- Bravimemes/patch.js
+++ Bravimemes/patch.js
@@ -5,8 +5,5 @@
 		"nmlgc/western_name_order",
 		"thpatch/lang_en"
 	],
-	"fonts": {
-		"RussellSquare.ttf": true
-	},
 	"title": "Okina memes and all that comes in my mind... Don't take it too seriously or badly."
 }
