9ea3e089e60bb9baad4d245c03832cf777f6143b
BraviNoname <braviaggron@gmail.com>
2019-02-25 11:20:45 +0100

    density fix

--- Bravimemes/th16/spells.js
+++ Bravimemes/th16/spells.js
@@ -23,7 +23,7 @@
     "113": "Lunatic Secret \"Gun of the Original Rabbit\"",
     "114": "Secret Dream \"Seal Barrage of Non Existence\"",
     "115": "Secret Stars \"Shining Seven Sapphires\"",
-    "116": "Secret Enemies \"Reckless Comets VS. Lunar Danmaku\"",
+    "116": "Secret Enemies \"Reckless Meteors VS. Lunar Danmaku\"",
     "117": "\"Mirage Split Sword\"",
     "118": "\"The Secret Human who Never Was\""
 }
