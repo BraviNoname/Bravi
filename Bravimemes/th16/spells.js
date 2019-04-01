69fdc8e733190df8dfa3f8b3ed3b11ed40de3df2
BraviNoname <braviaggron@gmail.com>
2019-04-01 02:22:16 +0200

    one last fix

--- Bravimemes/th16/spells.js
+++ Bravimemes/th16/spells.js
@@ -22,7 +22,7 @@
     "111": "Native Secrecy \"Double Snake and the Crawling Frogs\"",
     "112": "Reversed Secret \"Reversal Bow of Back & Forth\"",
     "113": "Secret Spirit \"Pseudo-Homing Amulet Barrage\"",
-    "114": "Lunatic Secret \"Illusionary Bullet Warp of the Original Rabbit\"",
+    "114": "Lunatic Secret \"Four Directions Mesmerism of the Original Rabbit\"",
     "115": "Secret Stars \"Shining Seven Sapphires\"",
     "116": "Secret Enemies \"Reckless Stars VS. Lunar Danmaku\"",
     "117": "\"Mirage Split Back Sword\"",
