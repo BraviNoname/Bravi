e9982f26af75f0d4f127442573a7a3ade32b5b4c
BraviNoname <braviaggron@gmail.com>
2019-04-06 17:08:26 +0200

    test

---
+++ Bravimemes/th165/titlemsg.js
@@ -0,0 +1,523 @@
+{
+    "0_1": "I can't write a diary without any photos.",
+    "0_2": [
+        "That was a take-down but no photos were taken...",
+        "I need to do it again sometime."
+    ],
+    "0_3": [
+        "Unlock Conditions: Cleared Nightmare Diary, Dream 1",
+        "& Cleared over 50 scenes in total"
+    ],
+    "0_4": [
+        "Unlock Conditions: Cleared Nightmare Diary, Dream 2",
+        "& Cleared over 60 scenes in total"
+    ],
+    "0_5": [
+        "Unlock Conditions: Cleared Nightmare Diary, Dream 3",
+        "& Cleared over 70 scenes in total"
+    ],
+    "0_6": [
+        "I haven't taken any photos of this dream.",
+        "Time to do it again."
+    ],
+    "1_0": [
+        "It feels like something",
+        "mysterious might happen..."
+    ],
+    "1_1": [
+        "I ran into Reimu-cchi, but she was acting really weird.",
+        "I mean, she was a pushover and all, but she still attacked me!"
+    ],
+    "1_2": [
+        "The photos are still in my camera roll,",
+        "so I guess it wasn't just a regular dream."
+    ],
+    "2_0": [
+        "Mondays are so depressing.",
+        "I hope I can at least have fun in my dreams."
+    ],
+    "2_1": [
+        "A rabbit-eared girl was pounding mochi.",
+        "Total LMAO am I right??"
+    ],
+    "2_2": [
+        "What's with this dango?",
+        "...Omigosh, it's SO tasty! It's a whole new flavor!"
+    ],
+    "2_3": [
+        "LOL she's shooting bullets from her sides, girl wyd??",
+        "Something this weird-looking is just begging for me to post it."
+    ],
+    "2_4": [
+        "It's a bunch of tri-color fruit dango on skewers.",
+        "Grape, strawberry, and... soy flavor? Why."
+    ],
+    "3_0": [
+        "They say that Sazae-san used",
+        "to air on Tuesdays, too..."
+    ],
+    "3_1": [
+        "A HUGE butterfly!",
+        "This is gonna get me so many views!"
+    ],
+    "3_2": [
+        "I remember learning that \"butterfly dream\" story in Chinese Lit class.",
+        "That's, like, totally the situation I'm in right now."
+    ],
+    "3_3": [
+        "These bullets are SO annoying!",
+        "That butterfly didn't get as many views as I thought either. UGH."
+    ],
+    "4_0": [
+        "Your mind goes totally blank on Wednesdays.",
+        "Just trudging through whatever tasks are in front of you."
+    ],
+    "4_1": [
+        "Yeah, I should've just used teleportation in the first place.",
+        "Don't want to use it too much, though. It gives me chills."
+    ],
+    "4_2": [
+        "I got attacked by a jizo statue...",
+        "Have I done anything unlucky lately?"
+    ],
+    "4_3": [
+        "That jizo statue stomped on me!",
+        "Ugh, this SUCKS! I'm gonna expose her on the internet!"
+    ],
+    "4_4": [
+        "There was something inside that huge bullet!",
+        "It had some kind of creature inside it! No, for real!"
+    ],
+    "5_0": [
+        "Thursdays are kind of quietly exciting.",
+        "Maybe because Friday comes after?"
+    ],
+    "5_1": [
+        "Hurling knives at people counts as danmaku?!",
+        "She's just some crazy weirdo, isn't she?"
+    ],
+    "5_2": [
+        "She's a \"yamanba,\" apparently. Talk about a super-endangered species!",
+        "Like, can you GET any more \"old-timey fairy tale?\" LOL"
+    ],
+    "5_3": [
+        "Uh, knives? Dangerous? Hello?",
+        "She'd be in jail IRL. Or a pro magician. One of the two."
+    ],
+    "6_0": [
+        "Friday at last!",
+        "Here's hoping these long, weird dreams will die down soon, too."
+    ],
+    "6_1": [
+        "There's cherry trees blooming... hey, what month was it, again?",
+        "Weird. I can't quite remember..."
+    ],
+    "6_2": [
+        "Wound up taking some photos that look kinda snake-like.",
+        "I'd say this moment is... shareable! Yeah, TOTALLY shareable!"
+    ],
+    "6_3": [
+        "Now that I get a closer look, she's got little horns on her head!",
+        "People always think these ones are just Halloween photos."
+    ],
+    "7_0": [
+        "Saturday! The best day of the week!",
+        "I feel totally great. No more nightmares for me!"
+    ],
+    "7_1": [
+        "Buh! I was just dreaming...? I feel like I had a really bizarre dream,",
+        "but my memory of the conversation is all fuzzy..."
+    ],
+    "8_0": [
+        "Let's see. I started having these strange",
+        "dreams on Sunday night, one week ago."
+    ],
+    "8_1": [
+        "So, like, all the dream-world residents are gonna come attack me?",
+        "These dream weirdos are all such loudmouths, honestly."
+    ],
+    "8_2": [
+        "W-woah, it got way harsher.",
+        "I know I'm just dreaming, but if I hit too many bullets, can I still die?"
+    ],
+    "8_3": [
+        "Noooo, I can't eat another biiite... mumble mumble...",
+        "Guh! It was just a dream?! ...Well, yeah. Figures."
+    ],
+    "8_4": [
+        "Hmm. Maybe the problem is that it's an insect photo in the first",
+        "place, butterfly or no? In terms of, like, social media etiquette."
+    ],
+    "8_5": [
+        "Against my expectations, the jizo photos were",
+        "pretty well-received. Thank you, thank you!"
+    ],
+    "8_6": [
+        "On closer look, she's not throwing kitchen knives, is she?",
+        "Don't they look more like the little Pop-Up Pirate knives?"
+    ],
+    "8_7": [
+        "Photos of dogs and cats only ever get nice comments.",
+        "Maybe they have the ability to control humans' brains."
+    ],
+    "9_0": [
+        "Ugh, so depressing.",
+        "I wish there'd never be another Monday."
+    ],
+    "9_1": [
+        "Where is this...?",
+        "There's these crater-like things in the photos."
+    ],
+    "9_2": [
+        "I got shot with a laser from a really far-away spot!",
+        "What do you call these? \"Blind-run killers?\" Lousy clown."
+    ],
+    "9_3": [
+        "If I could get a photo of what the laser sources look like,",
+        "I bet I'd get a ton of views!"
+    ],
+    "9_4": [
+        "This would be a lot harder <i$without> the rotating moon...",
+        "Is that, like, her way of being nice?"
+    ],
+    "10_0": [
+        "If you don't check the TV, you kind",
+        "of lose track of what day it is."
+    ],
+    "10_1": [
+        "That's a really fashionable-looking skirt... um, I think?",
+        "I don't get fashion."
+    ],
+    "10_2": [
+        "When I look closely, there's a whole bunch of orbs",
+        "in these photos like the ones Reimu-cchi has!"
+    ],
+    "10_3": [
+        "She looks like she wants me to take her picture,",
+        "but when I do, she goes and counterattacks. Yikes..."
+    ],
+    "10_4": [
+        "Geeze, this lady's scary. She just glides right up to you all silently...",
+        "C'mon, at least say 'hi' or something!"
+    ],
+    "11_0": [
+        "It feels like there's a lot more \"Wednesday such-and-such\"",
+        "names than other days of the week."
+    ],
+    "11_1": [
+        "Her again...",
+        "Is she on my side or not...?"
+    ],
+    "11_2": [
+        "The bamboo's glowiiing!",
+        "It's a Princess Kaguya factory up in here!"
+    ],
+    "11_3": [
+        "Weird... I thought whirlpools occured because",
+        "the particles gather towards the center?"
+    ],
+    "11_4": [
+        "What?! These movements are total nonsense, but you can't tell from just",
+        "a photo! A video would be better, but I can't erase bullets with that..."
+    ],
+    "11_5": [
+        "This one's pretty tough, but a photo doesn't convey the difficulty...",
+        "This sort of thing really ticks you off just a little bit."
+    ],
+    "11_6": [
+        "They sure have some weird hats. What were those called?",
+        "They're like those old-style \"delinquent kid\" haircuts... sorta."
+    ],
+    "12_0": [
+        "I wonder why the hospitals have",
+        "their day off on Thursday so often."
+    ],
+    "12_1": [
+        "The flames look like they're frozen in weird shapes...!",
+        "Is this an optical illusion?"
+    ],
+    "12_2": [
+        "I got a photo of a whole lot of vengeful spirits! Although there's",
+        "so many, it doesn't even look like a spirit photo at this point."
+    ],
+    "12_3": [
+        "Huh? Was her hair that color before?",
+        "It looks different from the other photos... is she wearing a wig?"
+    ],
+    "12_4": [
+        "So briiight! But it sure is pretty... I wish I could stand",
+        "in the center somehow and take pictures of all the geometric light."
+    ],
+    "12_5": [
+        "The moon fell outta the sky! It's a Giant Impact!",
+        "I got a super-crazy photo!!"
+    ],
+    "13_0": [
+        "Man, this week was sooo long.",
+        "Friday nights are just the best."
+    ],
+    "13_1": [
+        "I got a big circle mark! From some lady dressed like",
+        "one of those smart teachers from a long time ago."
+    ],
+    "13_2": [
+        "Photos with nice, regular patterns like this get",
+        "received really well. 'Cause everyone's simple-minded."
+    ],
+    "13_3": [
+        "P-pretty... um, I think you could call it that.",
+        "There's a fine line between beautiful and bizarre, huh."
+    ],
+    "13_4": [
+        "This lady seems like she's got a real harsh personality.",
+        "Kinda hard to approach..."
+    ],
+    "13_5": [
+        "She gave me another big circle mark!",
+        "...Wait, if I couldn't teleport, I think I'd be dead right now?"
+    ],
+    "14_0": [
+        "I'm getting pretty used to these nightmares.",
+        "Not like I <i$want> to have them, of course."
+    ],
+    "14_1": [
+        "Is she having fun throwing needles towards herself?",
+        "What a masochist."
+    ],
+    "14_2": [
+        "Quit aiming at me from below! (I am a girl, let's not forget.)",
+        "What's with this lady? She's got, like, no manners!"
+    ],
+    "14_3": [
+        "She sure seems to be having fun shooting danmaku from the",
+        "comfort of her chair. Bet she doesn't get enough exercise."
+    ],
+    "14_4": [
+        "Woah, she seems like she's got a real meticulous personality.",
+        "I feel like danmaku photos can show you someone's personality, too."
+    ],
+    "14_5": [
+        "It's hard to tell from just a photo,",
+        "but this one looked like a biohazard symbol."
+    ],
+    "14_6": [
+        "She taaalked! The haughty chair lady talked!",
+        "That aside, though, I can feel so much power flowing from my back!"
+    ],
+    "15_0": [
+        "Wonder if I'll have more nightmares this week...?",
+        "Well, let's see."
+    ],
+    "15_1": [
+        "It's a pair of vampire sisters.",
+        "I thought vampires didn't show up in photos, though..."
+    ],
+    "15_2": [
+        "Why, it's the Taoist and the head priest!",
+        "They're romping around way more than usual."
+    ],
+    "15_3": [
+        "A vampire and a head priest is a pretty weird combination.",
+        "They're weak to crosses, but I guess they're fine with sutras?"
+    ],
+    "15_4": [
+        "This light's as bright as the sun,",
+        "but she looks fine. Even though she's a vampire."
+    ],
+    "15_5": [
+        "Apparently, this vampire is extremely inquisitive, and will try",
+        "anything at least once. I wonder if she's a dojo member too?"
+    ],
+    "15_6": [
+        "Miss Hijiri really is something. She even got a shut-in vampire to spend",
+        "time outside. I wish I could get some shots of her in her biker getup, too..."
+    ],
+    "16_0": [
+        "I've been thinking about nothing but dreams...",
+        "Who cares about some dumb Monday."
+    ],
+    "16_1": [
+        "Let's see, this one is... oh my gosh, the Yama herself?!",
+        "Dang. The Gensokyo Chronicle really does have an article on everything."
+    ],
+    "16_2": [
+        "These must be the people who Reimu-cchi calls her rivals. If I were",
+        "to compare them... the Hakurei Shrine is, um. Kind of the lame one?"
+    ],
+    "16_3": [
+        "You can tell just from the photos that these two didn't go well together...",
+        "They're both real 'going my own way' types. Why did they team up?"
+    ],
+    "16_4": [
+        "Bullets that smash through the beautifully-arranged danmaku as they",
+        "proceed...? Might be a pretty good metaphor for court proceedings, huh."
+    ],
+    "16_5": [
+        "You can tell from the photos that they're a pretty similar pair.",
+        "Still, though... the bullet density is preeetty thick..."
+    ],
+    "16_6": [
+        "It smashed into the pillar and the pillar vanished?!",
+        "MAYDAY! There's been a terrible accident! I gotta post this right away!"
+    ],
+    "17_0": [
+        "What did I usually do in the",
+        "middle of weekdays, anyway...?"
+    ],
+    "17_1": [
+        "There she is! Gensokyo's secret character!",
+        "Apparently she stays cooped up indoors all the time."
+    ],
+    "17_2": [
+        "Ahh, this pair...! These two were rampaging all over",
+        "during the Perfect Possession thing! I'd better run!"
+    ],
+    "17_3": [
+        "Seems like they're keeping a distance from each other. A Lunarian and",
+        "a celestial... I wonder what their impression of each other is."
+    ],
+    "17_4": [
+        "Behold, the fairy-tale duo! They're as corny as a cell phone commercial,",
+        "and guaranteed to get shared across every social media feed there is!"
+    ],
+    "17_5": [
+        "I really wanna catch that little one...",
+        "She's not very photogenic. All the bullets around her look way too big..."
+    ],
+    "17_6": [
+        "Is it just me, or does this one make it look like the celestial",
+        "is whacking Princess Kaguya on the head over and over...?"
+    ],
+    "18_0": [
+        "This is weird...",
+        "All I'm thinking about is dreams, dreams, dreams."
+    ],
+    "18_1": [
+        "Gyaaah, she's reading my mind!",
+        "I'm so embarassed I might die! Better just kill her!"
+    ],
+    "18_2": [
+        "Oh, Koishi...",
+        "You found a weird animal, didn't you?"
+    ],
+    "18_3": [
+        "There's a bunch of hearts getting stolen, hearts getting foisted on me...",
+        "I'm not sure I get it, but animal hearts sure are tasty."
+    ],
+    "18_4": [
+        "A crow and a fox. Is there anyone who would like these photos...?",
+        "--Woah, speak of the devil! The Fluffy Fur Patrol is eating these pics up!"
+    ],
+    "18_5": [
+        "Can Koishi's sister not read the minds of animals?",
+        "She's got a metric ton of hearts jabbed into her, at any rate..."
+    ],
+    "18_6": [
+        "This danmaku was really straightforward, kind of at odds",
+        "with their characters. I'd hardly even call it a nightmare..."
+    ],
+    "19_0": [
+        "I wonder who",
+        "I'll meet today."
+    ],
+    "19_1": [
+        "These tiny stick figures are super scary, but...",
+        "From here, it just looks like I doodled on the photo."
+    ],
+    "19_2": [
+        "T-taiko drums?! And lightning?",
+        "Man, anything goes when it comes to danmaku."
+    ],
+    "19_3": [
+        "If I had to say one way or another, tanuki are probably a \"percussion\"",
+        "sort of animal. What with the belly drumming and all."
+    ],
+    "19_4": [
+        "Nooo, not lightning! My phone's gonna get fried!",
+        "And that weird wiggly light seems bad for electronics, too."
+    ],
+    "19_5": [
+        "The stick figures will even survive getting hit by lightning.",
+        "Um, seriously, scary much?"
+    ],
+    "19_6": [
+        "On closer inspection, some of the drums have skull marks drawn on them.",
+        "Is that just how expensive they are?"
+    ],
+    "20_0": [
+        "I can't remember what I used to dream about",
+        "before nightmares any more, but oh well!"
+    ],
+    "20_1": [
+        "Let's see, we've got Mokou, and... an oni? The \"headlong charge\" duo.",
+        "Basic poster children for really thick-headed attacks."
+    ],
+    "20_2": [
+        "A \"harsh teacher and delinquent student\" duo. Although lately,",
+        "the word \"delinquent\" seems to be referring to people like me."
+    ],
+    "20_3": [
+        "A weird duo with way-too-unique danmaku.",
+        "But it's perfect for teleportation practice!"
+    ],
+    "20_4": [
+        "There's this immortal jellyfish called a \"scarlet jellyfish.\"",
+        "Mokou...? Are immortality and scarlet related to each other...?"
+    ],
+    "20_5": [
+        "Thanks to this mist the oni made, I can't see the lasers at all...",
+        "The Hell duo is awful! Just plain awful!"
+    ],
+    "20_6": [
+        "I can feel an angry sort of aura coming off these photos.",
+        "Those two must really have a grudge against something in particular."
+    ],
+    "21_0": [
+        "So it's been another week...",
+        "It ought to be over soon, yeah."
+    ],
+    "21_1": [
+        "Danmaku from every direction!",
+        "Now this is what I call danmaku freestyle!"
+    ],
+    "21_2": [
+        "Ah, it's the usual duo~.",
+        "Their attacks are awful, but it's sorta relaxing to see them."
+    ],
+    "21_3": [
+        "Wait, am I being a sneak photographer right now?",
+        "I'd better be careful not to get flamed for this..."
+    ],
+    "21_4": [
+        "That massive laser is so awesome...",
+        "I bet it's amazing for stress relief."
+    ],
+    "21_5": [
+        "Quit aiming at me from below already!",
+        "Her lack of common sense infected Marisa-cchi too."
+    ],
+    "21_6": [
+        "I got yelled at not to take pictures without permission...",
+        "Interesting photos ought to be shared with people, shouldn't they?"
+    ],
+    "22_0": [
+        "There's still something left...!",
+        "Something that even I forgot about!"
+    ],
+    "22_1": [
+        "She says she's the ruler of dreams, but her attacks are really gentle...",
+        "Is she an enemy, or an ally...?"
+    ],
+    "22_2": [
+        "She sure is throwing my cards really dynamically.",
+        "Zener cards aren't cheap, you know!"
+    ],
+    "22_3": [
+        "Huh...? Wait, I was a dream world resident all along?",
+        "I think I've lost track of what's happening."
+    ],
+    "22_4": [
+        "I snagged a photo of the moment when I finished off the fake me!",
+        "Behold! This is proof that I truly am the real Sumireko Usami!"
+    ]
+}
