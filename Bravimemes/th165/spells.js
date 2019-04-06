537aef039922065569aee097c1f6782e204ddb61
BraviNoname <braviaggron@gmail.com>
2019-04-06 17:57:56 +0200

    more accurate I guess ?

--- Bravimemes/th165/spells.js
+++ Bravimemes/th165/spells.js
@@ -1,10 +1,10 @@
 {
     "51": "Secret Darkness \"Nightmare of the Dark Drown\"",
-    "52": "Surprise Secret \"Sudden Drop to the Uprising Rain\"",
+    "52": "Surprise Secret \"Sudden Drop and the Uprising Rain\"",
     "53": "Light in the Back \"Nightmarish Stars of an Absolutely Secret Human\"",
     "54": "Secret Illusion \"Hidden Bullets of the Dream Rabbit\"",
     "55": "Secret Lock \"Pseudo-Time Rewind Bind\"",
-    "93": "\"Lunatic Secret Barrier\"",
+    "93": "\"Secret Human Barrier\"",
     "95": "Union \"Sneak-Photographer Exorcism from the Back\"",
     "97": "Union \"Coward's Master Spark\""
 }
