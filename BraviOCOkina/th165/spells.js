61a9017fd662232cf71ef4a34ef1a17d18c05ccb
BraviNoname <braviaggron@gmail.com>
2019-04-09 23:12:10 +0200

    more precisions

--- BraviOCOkina/th165/spells.js
+++ BraviOCOkina/th165/spells.js
@@ -1,5 +1,5 @@
 {
-    "51": "Secret Darkness \"Nightmare of the Dark Drown\"",
+    "51": "Secret Darkness \"Drowning Nightmare into the Dark Whirlpool\"",
     "52": "Surprise Secret \"Sudden Drop and the Uprising Rain\"",
     "53": "Light in the Back \"Nightmarish Stars of an Absolutely Secret Human\"",
     "54": "Secret Illusion \"Hidden Bullets of the Dream Rabbit\"",
--- BraviOCOkina/th165/titlemsg.js
+++ BraviOCOkina/th165/titlemsg.js
@@ -1,26 +1,38 @@
 {
     "14_1": [
-        "Did she want me to sink into that darkness along with her?",
-        "She's kinda suicidal."
+        "She wanted me to drown in the darkness she cast on herself?",
+        "She looked despaired that way."
     ],
     "14_2": [
-        "Quit aiming at me from below! (I am a girl, let's not forget.)",
+        "Quit raining from below! (I am a girl, let's not forget.)",
         "What's with this girl? She's got, like, no manners!"
     ],
     "14_3": [
-        "She sure seems to be having fun shooting danmaku from the",
-        "comfort of her chair. Bet she doesn't get enough exercise."
+        "She makes her light shine in contrast to the dark.",
+        "She doesn't get enough exercise from her comfort, does she?"
     ],
     "14_4": [
-        "Woah, for a second she reminded me of the lunar rabbits.",
+        "The way she shoots these bullets reminds me of lunar rabbits.",
         "I feel like danmaku photos can show you someone's origins, too."
     ],
     "14_5": [
-        "It's hard to tell from just a photo,",
-        "everything in this danmaku felt reversed, backwards. "
+        "It's hard to tell from just a photo, this danmaku looked",
+        "like time rewinds itself along with the bullet movement."
     ],
     "14_6": [
         "She taaalked! She revealed herself as a human and... fellow outsider...?",
         "That aside, though, I can feel so much power flowing from my back!"
+    ],
+    "21_1": [
+        "Danmaku from every direction! Now this is what I call",
+        " danmaku freestyle! They do not seem to get along very well though."
+    ],
+    "21_3": [
+        "A union they say? What do these two have in common to be called so?",
+        "I'd better be careful not to get flamed for this ...sneak photography...?"
+    ],
+    "21_5": [
+        "Quit aiming at me from below already! Her lack of common",
+        "sense infected Marisa-cchi too, and they cooperate in unison so well."
     ]
 }
