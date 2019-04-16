25c4665bbee084178f640e13c6f7a27a55f83d3f
BraviNoname <braviaggron@gmail.com>
2019-04-16 12:33:14 +0200

    Nightmares

--- BraviOCOkina/th165/spells.js
+++ BraviOCOkina/th165/spells.js
@@ -4,7 +4,7 @@
     "53": "Hidden Light \"Nightmarish Stars of an Absolutely Secret Human\"",
     "54": "Secret Charms \"Covered Four Gods-Serving Maidens's Danmaku\"",
     "55": "Secret Time Lock \"Pseudo-Time Rewind Bind\"",
-    "93": "\"Secret Human Barrier\"",
-    "95": "Union \"Sneak-Photographer Exorcism from the Back\"",
-    "97": "Union \"Coward's Master Spark\""
+    "93": "Border Nightmare \"Starry Sapphire Barrier\"",
+    "95": "Nightmare Union \"Starry Sneak-Photographer Exorcism from the Back\"",
+    "97": "Nightmare Union \"Coward's Starry Master Spark\""
 }
