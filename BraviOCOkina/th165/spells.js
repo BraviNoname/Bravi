cab409114907c2675bc75603edf901523d47f53e
BraviNoname <braviaggron@gmail.com>
2019-04-16 16:38:22 +0200

    oh yea

--- BraviOCOkina/th165/spells.js
+++ BraviOCOkina/th165/spells.js
@@ -5,6 +5,6 @@
     "54": "Secret Charms \"Covered Four Gods-Serving Maidens's Danmaku\"",
     "55": "Secret Time Lock \"Pseudo-Time Rewind Bind\"",
     "93": "Border Nightmare \"Starry Sapphire Barrier\"",
-    "95": "Nightmare Union \"Starry Sneak-Photographer Exorcism from the Back\"",
+    "95": "Nightmare Union \"Sneak-Photographer Starry Exorcism from the Back\"",
     "97": "Nightmare Union \"Coward's Sapphire Master Spark\""
 }
