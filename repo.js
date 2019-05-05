2d0651a1237d97733066dad5aeda3d5598d3a66b
BraviNoname <braviaggron@gmail.com>
2019-05-05 12:23:19 +0200

    big oopsie

--- repo.js
+++ repo.js
@@ -4,7 +4,7 @@
 	"patches": {
 		"Bravimemes": "Memes that come in my mind... Like Metaklang's crapapella retexture.",
 		"BraviOCOkina": "Th16 Stage 6 and Extra Stage Hard Mode remaster, and 16.5 Okina with a character of mine.",
-		"Braviretex": "Bravi's Retexture Patch (for most of the main games)"
+		"Braviretex": "Bravi's Retexture Patch (for most of the main games)",
 		"BraviCOPlayer": "Mod that replaces some of the player sprites with that same character of the other patch."
 	},
 	"servers": [
