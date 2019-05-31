6a34be5363baf471a03784bb40b4d68980fe4d50
BraviNoname <braviaggron@gmail.com>
2019-05-31 20:20:41 +0200

    thinking and thinking

--- BraviOCOkina/th165/spells.js
+++ BraviOCOkina/th165/spells.js
@@ -1,8 +1,8 @@
 {
-    "51": "Hidden Darkness \"Consume the Darkness, Return it to Light\"",
-    "52": "Surprise Secret \"Sudden Drop and the Uprising Rain\"",
+    "51": "Hidden Balance \"Consume the Darkness, Return it to Light\"",
+    "52": "Secret Surprise \"Torrential Rain of the Drop Back Shot\"",
     "53": "Hidden Starlight \"Dream Stars of a Human of the Hidden Side\"",
-    "54": "Secret Charms \"Covered Four Gods-Serving Maidens's Danmaku\"",
+    "54": "Secret Mystic \"Netherworld Butterflies in Four Seasons\"",
     "55": "Secret Time Lock \"Pseudo-Time Rewind Bind\"",
     "93": "Secret Border Nightmare \"Starry Sapphire Barrier\"",
     "95": "Nightmare Union \"Sneak-Photographer Starry Exorcism from the Back\"",
