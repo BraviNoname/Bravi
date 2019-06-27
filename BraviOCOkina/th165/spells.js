7c8dd06a77090c3b5cfac2e65e76acf162487b61
BraviNoname <braviaggron@gmail.com>
2019-06-27 06:18:09 +0200

    this

--- BraviOCOkina/th165/spells.js
+++ BraviOCOkina/th165/spells.js
@@ -2,7 +2,7 @@
     "51": "Hidden Balance \"Consume the Darkness, Return it to Light\"",
     "52": "Secret Surprise \"Karakasa Tongue Under the Heavy Rain\"",
     "53": "Hidden Starlight \"Dream Stars of a Human of the Hidden Side\"",
-    "54": "Secret Four Elements \"Covered Fire, Air, Earth and Water\"",
+    "54": "Secret Four Elements \"Covert Fire, Air, Earth and Water\"",
     "55": "Secret Time Lock \"Pseudo-Time Rewind Bind\"",
     "93": "\"Nightmarish Starry Sapphire Barrier\"",
     "95": "\"Sneak-Photographer Starry Nightmarish Exorcism from the Back\"",
