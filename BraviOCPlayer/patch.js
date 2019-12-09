e94868e5e4d10cb7facda13adbf93ab6e0e75ed0
BraviNoname <braviaggron@gmail.com>
2019-12-09 08:49:27 +0100

    oh, also this

--- BraviOCPlayer/patch.js
+++ BraviOCPlayer/patch.js
@@ -3,5 +3,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/Bravi/BraviOCPlayer/"
 	],
-	"title": "(Th16, mostly) Post-Extra stage story mode player patch for the OC, replacing Reimu's story (endings aren't changed tho)."
+	"title": "(Th16, 17 mostly) Modded Reimu dialogues for the same OC. Player sprite modding affects all games."
 }
