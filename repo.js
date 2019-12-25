aab431bfdbf69af02d66706ce000457e4f15af9e
BraviNoname <braviaggron@gmail.com>
2019-12-25 11:56:19 +0100

    warning cauz no solution found

--- repo.js
+++ repo.js
@@ -4,7 +4,7 @@
 	"patches": {
 		"BraviOCOkina": "(Th16, 16.5) Replaces Okina with a new (OC) adversary in Stage 6, Extra (turned into Phantasm) and Wrong/Nightmare Saturdays.",
 		"BraviOCPlayer": "(Th16, mostly) Post-Extra stage story mode player patch for the OC, replacing Reimu's story (endings aren't changed tho).\n        Also please refrain from selecting this patch along with the OCOkina patch...",
-		"BraviOkina": "(Th16, 16.5) Same thing as the OCOkina Patch but Okina stays Okina.",
+		"BraviOkina": "(Th16, 16.5) UNSTABLE STAGE 6 SECOND SPELL! Same thing as the OCOkina Patch but Okina is the one using what the OC uses.",
 		"Bravimemes": "(Th11, 15) Memes that come in my mind... Like Subterranean Oparism or Legacy of Lunatic MetaPlate.",
 		"Braviretex": "(Th06 to 17 tsa games) Remake-like Retexture, Sounds, Dark UI and Display Fix Patch.",
 		"Teedee Arrows": "(Th15) Makes all bullets/lasers arrows. Typically deus rsf's nightmare."
