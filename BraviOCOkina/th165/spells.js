017a3e52ab382d9c384a200d24836c1bbcf698e8
BraviNoname <braviaggron@gmail.com>
2020-01-23 18:39:28 +0100

    ok

--- BraviOCOkina/th165/spells.js
+++ BraviOCOkina/th165/spells.js
@@ -2,7 +2,7 @@
     "51": "Sun Link \"Light's Artificial Dark-Consuming Sun\"",
     "52": "Surprise Link \"Karakasa Tongue Under the Heavy Rain\"",
     "53": "\"Starlight of All Seasons\"",
-    "54": "Four Elements Link\"Covert Fire, Air, Earth and Water\"",
+    "54": "Four Elements Link \"Covert Fire, Air, Earth and Water\"",
     "55": "Time Link \"Time-Rewinding Flower Chains\"",
     "93": "\"Nightmarish Star Barrier\"",
     "95": "\"United Shut-in-Exposer Back Star Exorcism\"",
