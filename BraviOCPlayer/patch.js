3f25659fba1aab81b4406886ebdc8079cceb35ae
BraviNoname <braviaggron@gmail.com>
2020-02-12 04:51:11 +0100

    ok

--- BraviOCPlayer/patch.js
+++ BraviOCPlayer/patch.js
@@ -3,5 +3,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/Bravi/BraviOCPlayer/"
 	],
-	"title": "(Th16, 16.5, 17 mostly) Modded (Reimu) dialogues for the same OC. Player sprite modding affects all games.\n            Also please refrain from selecting this patch along with the OCOkina patch..."
+	"title": "(Th15, 16, 16.5, 17 mostly) Modded (Reimu) dialogues for the same OC. Player sprite modding affects all games.\n            Also please refrain from selecting this patch along with the OCOkina patch..."
 }
