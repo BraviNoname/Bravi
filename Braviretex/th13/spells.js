f086d1a2c07108c0167c782f9a8814ba8e3b51d8
BraviNoname <braviaggron@gmail.com>
2020-02-17 05:57:34 +0100

    Create spells.js

---
+++ Braviretex/th13/spells.js
@@ -0,0 +1,7 @@
+{
+    "44": "Evil Sign \"Yang Xaogui\"",
+    "45": "Evil Sign \"Guhún Yegui\"",
+    "47": "Demonify \"Zouhuo Rùmó\"",
+    "51": "Possession \"Corpse Tóngji\"",
+    "53": "Spirit Link \"Tonglíng Yoshika\""
+}
