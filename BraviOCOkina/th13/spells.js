2e3f2f1061393ab375520bb220e97b9da791e673
BraviNoname <braviaggron@gmail.com>
2020-03-03 21:42:00 +0100

    yeah

--- BraviOCOkina/th13/spells.js
+++ BraviOCOkina/th13/spells.js
@@ -5,10 +5,11 @@
     "62": "Snipe Sign \"Sharp Shooting\"",
     "64": "Snipe Sign \"Relentless Sharp Shooting\"",
     "66": "Berserk \"Wrath of the Rear Face\"",
+    "68": "Berserk \"Merciless Furies of the Back\"",
     "70": "Paper Sign \"Shuffled Card Curtain\"",
     "72": "Paper Sign \"Slippery Card Curtain\"",
     "74": "Shotlock \"Starry Hindrance\"",
     "123": "Book Sign \"Maze of a Million Pages\"",
     "124": "Shotlock \"Endless Hindrance of the Stars\"",
-    "158": "\"Clock Ticking Towards to the Future\""
+    "158": "\"Clock Ticking Towards the Future\""
 }
