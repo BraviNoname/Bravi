4862fbfffa8d6c5fc58e58f30d3a889bb9f25072
BraviNoname <braviaggron@gmail.com>
2020-05-21 01:24:18 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -2,6 +2,7 @@
 	"contact": "Bravihood#9568",
 	"id": "Bravi",
 	"patches": {
+		"BraviKHBGM": "(Th12, 15) Some final and Extra boss themes got a Kingdom Hearts theme playing while you fight them.",
 		"BraviOCOkina": "(Th16, 16.5) Replaces Okina with a new (OC) adversary in Stage 6, Extra (turned into Phantasm) and Wrong/Nightmare Saturdays.",
 		"BraviOCPlayer": "(Th16, mostly) Post-Extra stage story mode player patch for the OC, replacing Reimu's story (endings aren't changed tho).\n        Also please refrain from selecting this patch along with the OCOkina patch...",
 		"BraviOkina": "(Th16, 16.5) UNSTABLE STAGE 6 SECOND SPELL! Same thing as the OCOkina Patch but Okina is the one using what the OC uses.",
