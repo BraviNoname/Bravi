be1d9955f0aa38c34c9964d10812aa066f730f53
BraviNoname <braviaggron@gmail.com>
2020-05-21 01:17:02 +0200

    ok

---
+++ BraviKHBGM/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+	],
+	"id": "Bravimemes",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/BraviKHBGM/"
+	],
+	"title": "(Th12, 15) Some final and Extra boss themes got a Kingdom Hearts theme playing while you fight them."
+}
