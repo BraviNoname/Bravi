05819729c44469f7a7238e1a257ef0c6661c5118
BraviNoname <braviaggron@gmail.com>
2020-06-03 13:01:10 +0200

    text info

--- BraviOCOkina/th165/spells.js
+++ BraviOCOkina/th165/spells.js
@@ -1,7 +1,7 @@
 {
     "51": "Sun Link \"Light's Artificial Dark-Consuming Sun\"",
     "52": "Surprise Link \"Karakasa Tongue Under the Heavy Rain\"",
-    "53": "Mini-Game \"Hidden Prize in All Seasons\"",
+    "53": "Doll Link \"Hidden Shell Game in Five Dolls\"",
     "54": "Four Elements Link \"Covert Fire, Air, Earth and Water\"",
     "55": "Time Link \"Time-Rewinding Flower Chains\"",
     "93": "\"Nightmarish Star Barrier\"",
