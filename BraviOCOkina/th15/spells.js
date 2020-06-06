c938a28249851dc4984a4c3445387a6027e2bffa
BraviNoname <braviaggron@gmail.com>
2020-06-06 20:33:39 +0200

    Third spellcard added!

--- BraviOCOkina/th15/spells.js
+++ BraviOCOkina/th15/spells.js
@@ -3,5 +3,6 @@
     "2": "Starlight \"Pouring Starlight\"",
     "5": "Starlight \"Flooding Starlight\"",
     "6": "Book Sign \"Scattered Pages\"",
-    "9": "Book Sign \"Scattered Book Mess\""
+    "9": "Book Sign \"Scattered Book Mess\"",
+    "133": "Light Sign \"Bright Lights's Blizzard\""
 }
