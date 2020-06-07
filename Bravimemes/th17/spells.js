4a5f513f1d0bd1ea3ebe8129bd5c4ebcaec97717
BraviNoname <braviaggron@gmail.com>
2020-06-07 20:32:58 +0200

    hehe

--- Bravimemes/th17/spells.js
+++ Bravimemes/th17/spells.js
@@ -1,5 +1,7 @@
 {
-    "93": "Secret Ceremony \"Kurokumah Dukkha\"",
+    "68": "\"Lillies-Shaped Sculpting Art\"",
+    "70": "\"Murderous Lilly Creatures\"", 
+    "93": "Secret Ceremony \"Kurokomah Dukkha\"",
     "94": "\"Overflowing Epidemicity\"",
     "98": "\"Peerless Dark Pegasus\""
 }
