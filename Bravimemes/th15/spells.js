d984818d308f19b55a201717c317d0c33a97277a
BraviNoname <braviaggron@gmail.com>
2020-06-07 02:11:37 +0200

    ok done

---
+++ Bravimemes/th15/spells.js
@@ -0,0 +1,5 @@
+{
+    "82": "\"Murderous Summer Heat\"",
+    "98": "\"Overflowing Scoring\"",
+    "102": "\"Refining the Score to 1 Billion\""
+}
---
+++ Bravimemes/th17/spells.js
@@ -0,0 +1,5 @@
+{
+    "93": "Secret Ceremony \"Kurokumah Dukkha\"",
+    "94": "\"Overflowing Epidemicity\"",
+    "98": "\"Peerless Dark Pegasus\""
+}
