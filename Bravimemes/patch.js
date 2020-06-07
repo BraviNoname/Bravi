6bd2bb4a38620da7b04de4a2ae6818f203cd4997
BraviNoname <braviaggron@gmail.com>
2020-06-07 02:53:40 +0200

    Update patch.js

--- Bravimemes/patch.js
+++ Bravimemes/patch.js
@@ -9,5 +9,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/Bravi/Bravimemes/"
 	],
-	"title": "(Th10, 11, 15) Memes that come in my mind... Like Mountain of Stoners, Subterranean Oparism or Legacy of Lunatic MetaPlate."
+	"title": "(Th10, 11, 15, 16, 17) Memes that come in my mind, and cursed patterns too... Like Mountain of Stoners, Subterranean Oparism or Legacy of Lunatic MetaPlate."
 }
