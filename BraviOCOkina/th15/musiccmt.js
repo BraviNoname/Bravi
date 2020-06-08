e3638b327a092a0a62e4ef5537944f5e15f2d40b
BraviNoname <braviaggron@gmail.com>
2020-06-08 04:14:22 +0200

    k

--- BraviOCOkina/th15/musiccmt.js
+++ BraviOCOkina/th15/musiccmt.js
@@ -5,8 +5,8 @@
         "Touhou 16.5 First Week theme, playing as Sereinae's theme.",
         "",
         "This is the fifth time making a fight like this.",
-        "She's once again from another time, but she is being completely",
-        "carefree this time, hence the calm pace of this theme I've chosen.",
+        "She's once again in another timeline, but as her power barely has",
+        "Okina's blessing, I've chosen a calm and carefree theme for her.",
         "Also yeah, her name is similar to Seiran's. Good job guys for the idea!"
     ]
 }
