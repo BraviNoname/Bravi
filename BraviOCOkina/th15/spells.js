267c1f640dbfab2339c7ebd884fb2c8a1355849f
BraviNoname <braviaggron@gmail.com>
2020-06-13 04:11:34 +0200

    Update spells.js

--- BraviOCOkina/th15/spells.js
+++ BraviOCOkina/th15/spells.js
@@ -4,5 +4,6 @@
     "5": "Starlight \"Flooding Starlight\"",
     "6": "Book Sign \"Scattered Pages\"",
     "9": "Book Sign \"Scattered Book Mess\"",
-    "153": "Light Sign \"Bright Lights's Blizzard\""
+    "153": "Light Sign \"Bright Lights's Blizzard\"",
+    "157": "Link Sign \"The Bullet that Reflects my Origins\""
 }
