901a2b229f4d756aca15af353df3d268d41835a6
BraviNoname <braviaggron@gmail.com>
2020-06-27 13:56:15 +0200

    Update spells.js

--- Bravimemes/th15/spells.js
+++ Bravimemes/th15/spells.js
@@ -1,5 +1,5 @@
 {
     "82": "\"Murderous Summer Heat\"",
     "98": "\"Overflowing Scoring\"",
-    "102": "\"Refining the Score to 1 Billion\""
+    "100": "\"Refining the Score to 1 Billion\""
 }
