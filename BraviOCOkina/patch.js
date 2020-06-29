d4f108703ca637f6c95001c6f15b2d409dda492d
BraviNoname <braviaggron@gmail.com>
2020-06-29 17:44:15 +0200

    Update patch.js

--- BraviOCOkina/patch.js
+++ BraviOCOkina/patch.js
@@ -9,5 +9,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/Bravi/BraviOCOkina/"
 	],
-	"title": "(Th13, 14.3, 15, 16, 16.5) Replaces the following with a new (OC) adversary.\n            Okina in Stage 6, Extra (turned into Phantasm) that got a revamp,\n             Wrong/Nightmare Saturdays that got a revamp too\n             Yukari in Day 10\n            Futo and Tojiko in Stage 5\n             Seiran in Stage 1."
+	"title": "(Th13, 14.3, 15, 16, 16.5) Replaces the following with a new (OC) adversary.\n             A stage 6 midboss got added in th13, 15, and 16 as well.\n            Okina in Stage 6, Extra (turned into Phantasm) that got a revamp,\n             Wrong/Nightmare Saturdays that got a stage background revamp too\n             Yukari in Day 10\n            Futo and Tojiko in Stage 5\n             Seiran in Stage 1."
 }
