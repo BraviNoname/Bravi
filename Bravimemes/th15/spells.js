4f5e49bdfa48729437a698c46287091e9008afd6
BraviNoname <braviaggron@gmail.com>
2020-07-31 15:48:10 +0200

    long old curse

--- Bravimemes/th15/spells.js
+++ Bravimemes/th15/spells.js
@@ -1,5 +1,6 @@
 {
     "82": "\"Murderous Summer Heat\"",
     "98": "\"Overflowing Point Item Value\"",
-    "101": "\"Overflowing Point Item Value\""
+    "101": "\"Overflowing Point Item Value\"",
+    "115": "Moon \"Endless Lunatic Impact\""
 }
