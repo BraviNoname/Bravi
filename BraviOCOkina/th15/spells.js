c2c75908818d7ce111b142fa79d78e7a0f55edf7
BraviNoname <braviaggron@gmail.com>
2020-09-06 03:18:30 +0200

    alright, done

--- BraviOCOkina/th15/spells.js
+++ BraviOCOkina/th15/spells.js
@@ -1,7 +1,7 @@
 {
     "0": "Star Sign \"Sniper Star Mines\"",
-    "2": "Starlight \"Pouring Starlight\"",
-    "5": "Starlight \"Flooding Starlight\"",
+    "2": "Butterfly Sign \"Star-shaped Butterfly View\"",
+    "5": "Butterfly Sign \"Butterfly Tanabata View\"",
     "6": "Book Sign \"Scattered Pages\"",
     "9": "Book Sign \"Scattered Book Mess\"",
     "153": "Light Sign \"Bright Lights's Blizzard\"",
