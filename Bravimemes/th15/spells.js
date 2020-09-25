75222a260fd84fb5225c2a5917746b360dd2c375
BraviNoname <braviaggron@gmail.com>
2020-09-25 22:24:20 +0200

    Update spells.js

--- Bravimemes/th15/spells.js
+++ Bravimemes/th15/spells.js
@@ -1,6 +1,10 @@
 {
     "82": "\"Legacy of Lunatic Kingdom's Crash\"",
+    "90": "\"Behind Trembling, Shivering Star\"",
+    "94": "\"Pristine Fuckery\"",
     "98": "\"Overflowing Point Item Value\"",
     "101": "\"Overflowing Point Item Value\"",
+    "102": "Junk Sign \"Unpurely Junkery Hell\"",
+    "104": "Junk Sign \"A Pristine Junkery Hell\"",
     "115": "Moon \"Endless Lunatic Impact\""
 }
