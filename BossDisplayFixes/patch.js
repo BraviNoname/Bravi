8a053c511ee2856e8fac4efd6f37a33fb67ac2ba
BraviNoname <braviaggron@gmail.com>
2020-11-02 10:42:36 +0100

    no retexture, just a boss display fix patch

---
+++ BossDisplayFixes/patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en"
+	],
+	"id": "Braviretex",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/BossDisplayFixes/"
+	],
+	"title": "(Th10, 13 to 17, and 16.5) Same as Retexture but it's only a Boss Display Fix Patch."
+}
