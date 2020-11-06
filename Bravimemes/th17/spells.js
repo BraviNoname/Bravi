ef13997bbb15b34956e9054c661e3c626e8e4333
BraviNoname <braviaggron@gmail.com>
2020-11-06 15:29:20 +0100

    hahaa

--- Bravimemes/th17/spells.js
+++ Bravimemes/th17/spells.js
@@ -1,6 +1,13 @@
 {
+    "60": "Square Shape \"Square-Shaped Fake-Out Art\"",
+    "62": "Square Shape \"Square Creature Fake Out\"",
+    "64": "Circular Shape \"Exceeded Circle Sculpting Art\"",
+    "66": "Circular Shape \"Too-Much-Circle Creature\"",
     "68": "\"Lillies-Shaped Sculpting Art\"",
     "70": "\"Murderous Lilly Creatures\"", 
+    "72": "Haniwa \"Not-this-Idol Idol Sculpting Art\"",
+    "74": "Haniwa \"Not-this-Idol Creature\"",
+    "76": "\"Anarchy-Shaped Sculpting Art\"",
     "80": "\"GeomeTHICC Creature\"",
     "84": "\"Idola Dominance\"",
     "93": "Secret Ceremony \"Kurokomah Dukkha\"",
