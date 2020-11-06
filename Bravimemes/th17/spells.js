99c6195ae7fcef51f95ce18b414fb7e1608297e5
BraviNoname <braviaggron@gmail.com>
2020-11-06 02:32:41 +0100

    hahaa

--- Bravimemes/th17/spells.js
+++ Bravimemes/th17/spells.js
@@ -1,6 +1,7 @@
 {
     "68": "\"Lillies-Shaped Sculpting Art\"",
     "70": "\"Murderous Lilly Creatures\"", 
+    "80": "\"GeomeTHICC Creature\"",
     "84": "\"Idola Dominance\"",
     "93": "Secret Ceremony \"Kurokomah Dukkha\"",
     "94": "\"Overflowing Epidemicity\"",
