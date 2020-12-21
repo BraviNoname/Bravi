36a2a5e16b0878797f0e611e0689c296f89634c9
BraviNoname <braviaggron@gmail.com>
2020-12-21 14:33:06 +0100

    I forgot that

--- BraviOCOkina/patch.js
+++ BraviOCOkina/patch.js
@@ -12,5 +12,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/Bravi/BraviOCOkina/"
 	],
-	"title": "(Th13, 14.3, 15, 16, 16.5) Replaces the following with a new (OC) adversary.\n             A stage 6 midboss got added in th13, 15, and 16 as well.\n            Okina in Stage 6, Extra (turned into Phantasm) that got a revamp,\n             Wrong/Nightmare Saturdays that got a stage background revamp too\n             Yukari in Day 10\n            Futo and Tojiko in Stage 5\n             Seiran in Stage 1."
+	"title": "(Th13, 14.3, 15, 16, 16.5) Replaces the following with a new (OC) adversary.\n             A stage 6 midboss got added in th13, 15, and 16 as well.\n            Okina in Stage 6, Extra (turned into Phantasm) that got a revamp,\n             Wrong/Nightmare Saturdays that got a stage background revamp too\n             Yukari in Day 10\n            Futo and Tojiko in Stage 5\n             Seiran in Stage 1\n             Mayumi in stages 5 and 6, got new additions to Keiki's fight too."
 }
