9bbba87d59c8378b50a7328c5db9a0a1cc050561
BraviNoname <braviaggron@gmail.com>
2021-01-08 03:04:56 +0100

    junky

--- Bravimemes/th16/spells.js
+++ Bravimemes/th16/spells.js
@@ -1,4 +1,5 @@
 {
+    "20": "\"Can't put different spellcard names cauz thcrap and spell IDs\"",
     "70": "Back Sign \"Murderous Backdoor of the Secret God\"",
     "73": "Back Sign \"Muderous Backdoor of the Absolute Secret God\""
 }
