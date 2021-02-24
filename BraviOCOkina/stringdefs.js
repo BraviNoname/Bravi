be6d33d0fb476dc908c1ced0edb5cea77fbc49c6
BraviNoname <braviaggron@gmail.com>
2021-02-24 21:05:40 +0100

    Create stringdefs.js

---
+++ BraviOCOkina/stringdefs.js
@@ -0,0 +1,8 @@
+{
+    "th13": "Touhou Divine Spirit Mausoleum ~ Ten Desires (Alternate Stage 5 & 6)",
+    "th143": "Danmaku Amanojaku ~ Impossible Spell Card (Alternate Day 10)",
+    "th15": "Touhou Eastern Legend of the Ultramarine Orb ~ Legacy of Lunatic Kingdom (Alternate Stage 1 & 6)",
+    "th16": "Touhou Eastern Jade Scepter of the Skies ~ Hidden Star in Four Seasons (Alternate Stage 6 & Kaizo Extra)",
+    "th165": "Secret Sealing Nightmare Diary ~ Violet Detector (Alternate Wrong/Nightmare Saturday)",
+    "th17": "Touhou Eastern Beast in Oni's Shape ~ Wily Beast and Weakest Creature (Alternate Stage 5 & 6)",
+}
