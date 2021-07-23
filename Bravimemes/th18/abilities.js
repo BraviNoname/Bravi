aa012eed4790729c6f55e43dc8c34cf064743a88
BraviNoname <braviaggron@gmail.com>
2021-07-23 12:54:02 +0200

    lol

---
+++ Bravimemes/th18/abilities.js
@@ -0,0 +1,10 @@
+{
+    "48_0": [
+        "An ability card that summons Lily...Junko? when used.",
+        "She's an item carrier.",
+        "She either holds a Spell Card fragment or a life fragment.",
+        "This variant of Lily White is very agressive though!",
+        "The more times you summon her, the more white the screen becomes!"
+    ],
+    "48": "Murderous Item Season"
+}
