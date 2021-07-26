7d752fdf22984a2240e6aadc0ea0bf245b23cb75
BraviNoname <braviaggron@gmail.com>
2021-07-26 16:38:52 +0200

    ok

--- Bravimemes/th16/spells.js
+++ Bravimemes/th16/spells.js
@@ -1,3 +1,5 @@
 {
     "20": "\"I do not believe that it is statistically possible to capture any of these spells without using TAS, or cheating in some way. I kinda think that ZUN was trolling us by putting this pattern in the game, and having no way to tell if someone is cheating just by looking at the screen footage. I think he knows full well that it's impossible, and the whole thing is little more than a social experiment to see how many people will cheat and then try to pass their work off as legitimate.\"",
+    "82": "Pure Winter \"Purely Snowman Hell\"",
+    "84": "Pure Winter \"A Pristine Snowman Hell\""
 }
