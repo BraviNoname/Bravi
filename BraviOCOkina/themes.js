cb681e1f873da91d0d382e3d8c336afef326aff3
BraviNoname <braviaggron@gmail.com>
2021-07-30 23:07:03 +0200

    sudden change of mind

--- BraviOCOkina/themes.js
+++ BraviOCOkina/themes.js
@@ -1,6 +1,6 @@
 {
     "th13_11": "Lunatic Dreamer", 
-    "th15_03": "Lost Corpse",
+    "th15_03": "Distorted Division",
     "th16_12": "Deep Drop",
     "th16_14": "Man-made “Perfect and Wrong”",
     "th17_11": "Boundary War III"
