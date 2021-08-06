ac9857264fcf66cf0cdb1e7ec31a460838464227
BraviNoname <braviaggron@gmail.com>
2021-08-06 18:04:31 +0200

    yes

--- Bravimemes/th16/spells.js
+++ Bravimemes/th16/spells.js
@@ -1,5 +1,17 @@
 {
     "20": "\"I do not believe that it is statistically possible to capture any of these spells without using TAS, or cheating in some way. I kinda think that ZUN was trolling us by putting this pattern in the game, and having no way to tell if someone is cheating just by looking at the screen footage. I think he knows full well that it's impossible, and the whole thing is little more than a social experiment to see how many people will cheat and then try to pass their work off as legitimate.\"",
+    "70": "Back Sign \"Halo of the Murderous Lillies\"",
+    "73": "Back Sign \"Halo of the Absolutely Murderous Lillies\"",
+    "74": "Hidden Summer \"Scorch By Hot Summer In a Dome\"",
+    "76": "Hidden Summer \"Scorched Earth Dome of Abnormal Intense Heat\"",
+    "78": "Hidden Fall \"Die of Famine -South Hemisphere Version-\"",
+    "80": "Hidden Fall \"Preta of Abnormal Blight-South Hemisphere Version-\"",
     "82": "Pure Winter \"Purely Snowman Hell\"",
-    "84": "Pure Winter \"A Pristine Snowman Hell\""
+    "84": "Pure Winter \"A Pristine Snowman Hell\"",
+    "86": "Hidden Spring \"April Wizard-South Hemisphere Version-\"",
+    "88": "Hidden Spring \"Black Magician of Abnormal Falling Petals -South Hemisphere Version-\"",
+    "90": "\"Hidden Breezy Cherry Blossom -Reflect Version-\"",
+    "94": "\"Hidden Perfect Summer Ice -Hail Dance-\"",
+    "98": "\"Hidden Crazy Fall Wind -Tengu on Crack-\"",
+    "102": "\"Hidden Extreme Winter -You're too slow for this-\""
 }
