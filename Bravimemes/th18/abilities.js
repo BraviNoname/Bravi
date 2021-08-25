1d6ed6b0157137eba841dc9aed89d66cdee63e3b
BraviNoname <braviaggron@gmail.com>
2021-08-25 14:54:47 +0200

    ok

--- Bravimemes/th18/abilities.js
+++ Bravimemes/th18/abilities.js
@@ -1,10 +1,10 @@
 {
     "48_0": [
         "An ability card that summons Lily...Junko? when used.",
-        "She's an item carrier.",
-        "She either holds a Spell Card fragment or a life fragment.",
-        "This variant of Lily White is very agressive though!",
-        "The more times you summon her, the more white the screen becomes!"
+        "She's an item carrier, well, for those EXPLODING Lillies that is.",
+        "They all unleash Power, Money, Lives and Bombs.",
+        "Don't try to kill this version of Lily White, she's invincible and never leaves!",
+        "You may cheese the game with that card, the price to pay is big though!"
     ],
     "48": "Murderous Item Season"
 }
