a888cb1bce4a1c57ec997405bac602da2c8fa393
BraviNoname <braviaggron@gmail.com>
2021-09-03 17:22:01 +0200

    Initial release !

---
+++ BraviOCOkina/th18/spells.js
@@ -0,0 +1,10 @@
+{
+    "44": "Ability Card \"Miracle Mallet\"",
+    "46": "Ability Card \"Bountiful Miracle Mallet\"",
+    "48": "Ability Card \"Gale Geta\"",
+    "50": "Ability Card \"Graceful Gale Geta\"",
+    "52": "Ability Card \"Great Tengu's Barley Rice\"",
+    "54": "Ability Card \"Great Tengu's Delicious Barley Rice\"",
+    "56": "Ability Card \"The Shadow Behind Your Back\"",
+    "97": "Reality Shift \"Lurking Rainbow Stars of Hindrance\""
+}
