5de729b65fd3340beb5b01ae99a6eaba522a3aa1
BraviNoname <braviaggron@gmail.com>
2021-09-19 01:59:01 +0200

    Update stringdefs.js

--- BraviOCOkina/stringdefs.js
+++ BraviOCOkina/stringdefs.js
@@ -4,5 +4,11 @@
     "th15": "Touhou Eastern Legend of the Ultramarine Orb ~ Legacy of Lunatic Kingdom (Alternate Stage 1 & 6)",
     "th16": "Touhou Eastern Jade Scepter of the Skies ~ Hidden Star in Four Seasons (Alternate Stage 6 & Kaizo Extra)",
     "th165": "Secret Sealing Nightmare Diary ~ Violet Detector (Alternate Wrong/Nightmare Saturday)",
-    "th17": "Touhou Eastern Beast in Oni's Shape ~ Wily Beast and Weakest Creature (Alternate Stage 5 & 6)"
+    "th17": "Touhou Eastern Beast in Oni's Shape ~ Wily Beast and Weakest Creature (Alternate Stage 5 & 6)",
+    "th18": "Touhou Rainbow Dragon Cavern ~ Unconnected Marketeers (Alternate Stage 5 & 6)",
+    "th13_scorefile_fn": "scoreth13_Sereinae.dat",
+    "th15_scorefile_fn": "scoreth15_Sereinae.dat",
+	"th16_scorefile_fn": "scoreth16_Sereinae.dat",
+	"th17_scorefile_fn": "scoreth17_Sereinae.dat",
+    "th18_scorefile_fn": "scoreth18_Sereinae.dat"
 }
