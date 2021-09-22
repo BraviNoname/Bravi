f45cc65fec00140be447cc2066822aa8c1b94f35
BraviNoname <braviaggron@gmail.com>
2021-09-22 07:19:09 +0200

    OK

---
+++ BraviOCOkina/th18/musiccmt.js
@@ -0,0 +1,12 @@
+{
+    "11": [
+        "@",
+        "",
+        "This is a placeholder for Sereinae's theme.",
+        "Composed by Name Mean Amen. Original Name : Nogama's Whirlwind.",
+        "The wind sound effects I think fit the high altitude this battle takes place on.",
+        "Meanwhile, the melody has a childish, and yet menacing vibe to it too.",
+        "This time she's just fighting our protagonist for entertainment,",
+        "even if the incident is an inconvenience, she's still having fun at the moment."
+    ]
+}
