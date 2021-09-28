ec14c61d89e91e8cb7fa3cdf6a0dda51daf05503
BraviNoname <braviaggron@gmail.com>
2021-09-28 18:35:56 +0200

    ok done

--- BraviOCOkina/th18/musiccmt.js
+++ BraviOCOkina/th18/musiccmt.js
@@ -2,11 +2,11 @@
     "11": [
         "@",
         "",
-        "This is a placeholder for Sereinae's theme.",
+        "This is Sereinae's theme.",
         "Composed by Name Mean Amen.",
-        "The wind sound effects I think fit the high altitude this battle takes place on.",
-        "Meanwhile, the melody has a childish, and yet menacing vibe to it too.",
-        "This time she's just fighting our protagonist for entertainment,",
-        "even if the incident is an inconvenience, she's still having fun at the moment."
+        "This theme has leitmotifs of Okina's theme due to being her servant.",
+        "Since she's just fighting for fun, the theme isn't as emotional as Megumu's theme.",
+        "The whole reason behind this fight is entertainment, even if the incident",
+        "is an inconvenience for both sides."
     ]
 }
