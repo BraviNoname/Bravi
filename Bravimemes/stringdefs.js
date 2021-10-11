d46042a803a1e25932fa149179d1473e4fe6e629
BraviNoname <braviaggron@gmail.com>
2021-10-11 02:01:32 +0200

    Create stringdefs.js

---
+++ Bravimemes/stringdefs.js
@@ -0,0 +1,8 @@
+{
+    "th13_scorefile_fn": "scoreth13_Bravimemes.dat",
+    "th14_scorefile_fn": "scoreth14_Bravimemes.dat",
+    "th15_scorefile_fn": "scoreth15_Bravimemes.dat",
+	"th16_scorefile_fn": "scoreth16_Bravimemes.dat",
+	"th17_scorefile_fn": "scoreth17_Bravimemes.dat",
+	"th18_scorefile_fn": "scoreth18_Bravimemes.dat"
+}
