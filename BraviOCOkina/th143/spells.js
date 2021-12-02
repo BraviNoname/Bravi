215d7b393a0090a67f6c3df8e5244ef42ef8e67f
BraviNoname <braviaggron@gmail.com>
2021-12-02 23:00:19 +0100

    ok

--- BraviOCOkina/th143/spells.js
+++ BraviOCOkina/th143/spells.js
@@ -1,4 +1,4 @@
 {
-    "70": "\"The Four Chapters of a Replica's Life\"",
-    "75": "\"The Frustration of Being Only a Creation\""
+    "70": "\"The Four Chapters of a Fake's Life\"",
+    "75": "\"The Torments of the Outcast\""
 }
