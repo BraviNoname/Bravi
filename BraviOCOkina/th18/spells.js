9806c3ba304bfc87ed270e2350497543b081f2fa
BraviNoname <braviaggron@gmail.com>
2022-01-11 18:46:27 +0100

    Update spells.js

--- BraviOCOkina/th18/spells.js
+++ BraviOCOkina/th18/spells.js
@@ -5,7 +5,7 @@
     "50": "Ability Card \"Graceful Gale Geta\"",
     "52": "Ability Card \"Great Tengu's Barley Rice\"",
     "54": "Ability Card \"Great Tengu's Delicious Barley Rice\"",
-    "56": "Back Sign\"The Shadow Behind Your Back\"",
+    "56": "Back Sign \"The Shadow Behind Your Back\"",
     "97": "Reality Shift \"Lurking Rainbow Stars of Hindrance\"",
     "101": "Moon Link \"Rainbow Butterfly Under the Moon\"",
     "113": "\"Cheap Cards for a Cheap Price\"",
