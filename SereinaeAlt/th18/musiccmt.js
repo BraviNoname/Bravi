a278915057654d7ffbbd67bf31e355d2b58ca81c
BraviNoname <braviaggron@gmail.com>
2022-06-29 00:44:58 +0200

    staff roll added

--- SereinaeAlt/th18/musiccmt.js
+++ SereinaeAlt/th18/musiccmt.js
@@ -8,5 +8,15 @@
         "and the false structure being what she is in the end, the fake of many people in one.",
         "The theme has some silent part, it would show how forgetful, and yet peaceful person",
         "she is, until the theme starts to show her will to remember for good."
+    ],
+    "17": [
+        "@",
+        "",
+        "This is the staff roll theme.",
+		"Composed by Ichigo-Paprika.",
+        "",
+        "A fitting BGM for a slice of life story in Sereinae's hostel.",
+		"I wish for her to live inside of everyone else's memories forever,",
+		"no matter if her existence as an OC is forgettable."
     ]
 }
--- SereinaeAlt/themes.js
+++ SereinaeAlt/themes.js
@@ -1,3 +1,4 @@
 {
-    "th18_13": "Harmful Substance in False Structures"
+    "th18_13": "Harmful Substance in False Structures",
+    "th18_17": "The New Eighth Aerial Paradise City"
 }
