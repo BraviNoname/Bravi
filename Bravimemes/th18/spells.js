e68e1c80d8e8b8f3065bd25d994e0dce495fe36a
BraviNoname <braviaggron@gmail.com>
2022-07-17 15:53:10 +0200

    Create spells.js

---
+++ Bravimemes/th18/spells.js
@@ -0,0 +1,13 @@
+{
+    "68": "\"All My Bullets are Belong to You\"",
+    "70": "\"All My Bullets are Belong to You\"",
+    "71": "\"All My Danmaku are Belong to You\"",
+    "76": "\"Rainbow-Shaped Sculpting Art\"",
+    "78": "\"Rainbow-Shaped Sculpting Art\"",
+    "79": "\"Rainbow-Shaped Sculpting Art\"",
+    "80": "\"Asylum of Sanic Danmaku\"",
+    "60": "\"An Unquestionable Offering to the Ownerless\"",
+    "62": "\"An Offering to the Blinking Rainbow Fantasy Seal\"",
+    "64": "\"Readability Hoarder's Obsession\"",
+    "72": "\"Rainbow Ring of Drunk People\""
+}
