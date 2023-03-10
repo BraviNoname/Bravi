f1ed8235c6f9813b7f17270d50d7c34f4f286164
BraviNoname <braviaggron@gmail.com>
2023-03-10 22:52:09 +0100

    Dialogues ain't there now, might be added if people really want to

---
+++ Létoile/patch.js
@@ -0,0 +1,18 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en",
+		"Bravi/BossDisplayFixes"
+	],
+	"fonts": {
+		"RussellSquare.ttf": true
+	},
+	"id": "SereinaeAlt",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/Létoile/"
+	],
+	"title": "(Th16) Custom playable character shot type for Sereinae (Spring).",
+	"update": true
+}
