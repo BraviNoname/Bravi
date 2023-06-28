5c7bfa2b999d43fe8ae1ae6baaa454ffa0a5c340
BraviNoname <braviaggron@gmail.com>
2023-06-28 15:54:36 +0200

    Update trophy.js

--- BraviOCOkina/th17/trophy.js
+++ BraviOCOkina/th17/trophy.js
@@ -2,24 +2,24 @@
     "37_0": [
         "Find the Rare Beast Spirit in Stage 5!",
         "It appears if you defeat the midboss under certain conditions.",
-        "(Hint: When the enemy lowers her guard...)"
+        "(Hint: When the enemy takes a break...)"
     ],
     "38_0": [
         "Find the Rare Beast Spirit in Stage 6!",
         "It appears if you defeat the midboss under certain conditions.",
-        "(Hint: At the finish line!)"
+        "(Hint: Enemies at their finish line!)"
     ],
 
-    "37": "Looks like she dropped her book!",
+    "37": "She dropped her book!",
     "37_1": [
         "Found the Rare Beast Spirit in Stage 5, the Book of Memories.",
-        "Beat the midboss when she's taking a break!",
+        "Beat the midboss when she isn't shooting!",
         "The window is pretty short and gets shorter on higher difficulties."
     ],
-    "38": "A Shadow's Wish",
+    "38": "Spirit of her Other Self",
     "38_1": [
         "Found the Rare Beast Spirit in Stage 6, the Desire Spirit.",
         "Defeat the midboss after one enemy reaches the opposite side!",
-        "They always signal it when they reach their finish line."
+        "They always signal it when they do so by shooting white orbs."
     ]
 }
