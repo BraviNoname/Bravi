59a7237d1eb3b8b95be1dfb7a5f701d093bb8de0
BraviNoname <braviaggron@gmail.com>
2024-06-14 22:12:54 +0200

    mirror match

---
+++ Mirror Match/patch.js
@@ -0,0 +1,18 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en",
+		"Bravi/Letoile",
+		"Bravi/BraviOCOkina"
+	],
+	"fonts": {
+		"RussellSquare.ttf": true
+	},
+	"id": "BraviOCOkina",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/Mirror_Match/"
+	],
+	"title": "(Th16) Combines BraviOCOkina mod with the Létoile mod for a mirror match at st6/Extra with alternate dialogues."
+}
