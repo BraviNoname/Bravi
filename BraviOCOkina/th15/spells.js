27bbf52e0f379aec16df549c7aa0b61cd91640b1
BraviNoname <braviaggron@gmail.com>
2024-06-22 19:47:17 +0200

    Update spells.js

--- BraviOCOkina/th15/spells.js
+++ BraviOCOkina/th15/spells.js
@@ -5,5 +5,7 @@
     "6": "Book Sign \"Scattered Pages\"",
     "9": "Book Sign \"Scattered Book Mess\"",
     "153": "Light Sign \"Bright Lights's Blizzard\"",
-    "157": "Link Sign \"The Bullet that Reflects my Origins\""
+    "157": "Link Sign \"The Bullet that Reflects my Origins\"",
+    "98": "\"Chasing Blemishes\"",
+    "101": "\"Refinement of Lurking Earthen Impurity\""
 }
