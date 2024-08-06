bc2844cf647f1e44b97f98c180252ab0a14725e6
BraviNoname <braviaggron@gmail.com>
2024-08-06 22:53:17 +0200

    lol

---
+++ LetoilePlayerReskin/patch.js
@@ -0,0 +1,18 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en",
+		"Bravi/BossDisplayFixes"
+	],
+	"fonts": {
+		"RussellSquare.ttf": true
+	},
+	"id": "SereinaeAlt",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/LetoilePlayerReskin/"
+	],
+	"title": "(Every non-versus modern game) Just a lazy Sereinae Létoile reskin for all player sprites, just because she likes to cosplay. Not a custom shottype btw, that's for Létoile mod.",
+	"update": true
+}
