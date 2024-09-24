ba37a287e4c15d2a8b3ec6fd6dfb77e1b9bf026b
BraviNoname <braviaggron@gmail.com>
2024-09-25 01:53:51 +0200

    14.3

---
+++ Letoile/th143/spells.js
@@ -0,0 +1,6 @@
+{
+    "60": "Spirit Sign \"Pentagram-Shaped Racing Trail\"",
+    "61": "Limit \"Linked Sonic Blade\"",
+    "64": "Teamwork \"Partner Star-Ranked Cure\"",
+    "65": "Oni Crowd \"Hundred Oni Kaburo\""
+}
