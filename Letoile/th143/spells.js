c0d07c1ee2a70dcf46fea566f39be6c765b64546
BraviNoname <braviaggron@gmail.com>
2024-09-27 04:25:05 +0200

    new spellcard

--- Letoile/th143/spells.js
+++ Letoile/th143/spells.js
@@ -1,6 +1,6 @@
 {
-    "60": "Spirit Sign \"Pentagram-Shaped Racing Trail\"",
+    "60": "Miracle Spirit \"Pentagram-Shaped Racing Trail\"",
     "61": "Limit \"Linked Sonic Blade\"",
-    "64": "Teamwork \"Partner Star-Ranked Cure\"",
-    "65": "Oni Crowd \"Hundred Oni Kaburo\""
+    "64": "Recovery \"Partner Star-Ranked Cure\"",
+    "65": "Limit \"Ragnarok Danmaku\""
 }
