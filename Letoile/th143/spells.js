4d7dfaac7f8339d066bb7a7dca3825f097dba797
BraviNoname <braviaggron@gmail.com>
2024-09-27 05:20:22 +0200

    ok

--- Letoile/th143/spells.js
+++ Letoile/th143/spells.js
@@ -2,5 +2,5 @@
     "60": "Miracle Spirit \"Pentagram-Shaped Racing Trail\"",
     "61": "Limit \"Linked Sonic Blade\"",
     "64": "Recovery \"Partner Star-Ranked Cure\"",
-    "65": "Limit \"Ragnarok Danmaku\""
+    "65": "Limit \"Lightning Ragnarok Danmaku\""
 }
