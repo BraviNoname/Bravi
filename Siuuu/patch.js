9d52a805a97f528816e328f9c0954edd823103c6
BraviNoname <braviaggron@gmail.com>
2024-10-07 02:15:35 +0200

    SIUUU

---
+++ Siuuu/patch.js
@@ -0,0 +1,16 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en"
+	],
+	"fonts": {
+		"RussellSquare.ttf": true
+	},
+	"id": "BraviOCOkina",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/Siuuu/"
+	],
+	"title": "(All Touhou Windows STG) Replaces (se_)enep01.wav (boss death sound) by SIUUU from Sawano7's streams"
+}
