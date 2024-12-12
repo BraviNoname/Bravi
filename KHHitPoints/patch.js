91b3d1a2715b81bd87f42c0dbe45d547bbb5c77a
BraviNoname <braviaggron@gmail.com>
2024-12-12 16:06:03 +0100

    kh hit point mod online

---
+++ KHHitPoints/patch.js
@@ -0,0 +1,18 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en",
+		"Bravi/BossDisplayFixes"
+	],
+	"fonts": {
+		"RussellSquare.ttf": true
+	},
+	"id": "SereinaeAlt",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/KHHitPoints/"
+	],
+	"title": "(Every stg) Boss Lifebar and Life count reskin based on KH games and modern life bomb icon colors, from Braviretex.",
+	"update": true
+}
