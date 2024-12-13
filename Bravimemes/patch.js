5aa9f1dcacc96f9219a03147606632b3a9990cf7
BraviNoname <braviaggron@gmail.com>
2024-12-13 13:19:10 +0100

    Update patch.js

--- Bravimemes/patch.js
+++ Bravimemes/patch.js
@@ -14,5 +14,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/Bravi/Bravimemes/"
 	],
-	"title": "(Th10, 11, 15, 16, 17, 18) Memes that come in my mind, and cursed patterns too... Like Mountain of Stoners, Subterranean Oparism or Legacy of Lunatic MetaPlate.\n            Okina's fight has the order of the seasons shuffled, and Mai and Satono drop too many ressources.\n            Keiki and Saki also seem to be affected by a curse, making them wrathful."
+	"title": "(Th10 onwards) Memes that come in my mind, and cursed patterns too... Like Mountain of Stoners, Subterranean Oparism or Legacy of Lunatic MetaPlate.\n            Okina's fight has the order of the seasons shuffled, and Mai and Satono drop too many ressources.\n            Keiki and Saki also seem to be affected by a curse, making them wrathful. This list is non-exhaustive btw."
 }
