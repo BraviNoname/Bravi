57cf938c25b4e49dadfb264650443008106297b6
BraviNoname <braviaggron@gmail.com>
2024-12-21 18:04:25 +0100

    trying this

---
+++ Bravimemes+ExpHP/patch.js
@@ -0,0 +1,18 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en",
+        "ExpHP/base_exphp",
+        "ExpHP/anm_leak",
+        "ExpHP/sprite_death_fix",
+        "ExpHP/bullet_cap",
+		"32th/score_uncap"
+	],
+	"id": "Bravimemes",
+	"servers": [
+		"https://mirrors.thpatch.net/Bravi/Bravimemes+ExpHP/"
+	],
+	"title": "Bravimemes but with the ExpHP+32th mods dependencies. Select the other Bravimemes if it crashes when loading a stage."
+}
--- Bravimemes/patch.js
+++ Bravimemes/patch.js
@@ -3,16 +3,11 @@
 		"nmlgc/base_tsa",
 		"nmlgc/script_latin",
 		"nmlgc/western_name_order",
-		"thpatch/lang_en",
-"ExpHP/base_exphp",
-"ExpHP/anm_leak",
-"ExpHP/sprite_death_fix",
-"ExpHP/bullet_cap",
-		"32th/score_uncap"
+		"thpatch/lang_en"
 	],
 	"id": "Bravimemes",
 	"servers": [
 		"https://mirrors.thpatch.net/Bravi/Bravimemes/"
 	],
-	"title": "(Th10 onwards) Memes that come in my mind, and cursed patterns too... Like Mountain of Stoners, Subterranean Oparism or Legacy of Lunatic MetaPlate.\n            Okina's fight has the order of the seasons shuffled, and Mai and Satono drop too many ressources.\n            Keiki and Saki also seem to be affected by a curse, making them wrathful. This list is non-exhaustive btw."
+	"title": "(Th10 onwards) (Select Bravimemes+ExpHP to remove bullet caps and make crashes less frequent) Memes that come in my mind, and cursed patterns too... Like Mountain of Stoners, Subterranean Oparism or Legacy of Lunatic MetaPlate.\n            Okina's fight has the order of the seasons shuffled, and Mai and Satono drop too many ressources.\n            Keiki and Saki also seem to be affected by a curse, making them wrathful. This list is non-exhaustive btw."
 }
