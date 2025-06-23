87ee9944c0437727278a357bb0a8ecf00feeb775
BraviNoname <braviaggron@gmail.com>
2025-06-23 02:28:13 +0200

    not

--- BossDisplayFixes/files.js
+++ BossDisplayFixes/files.js
@@ -1,7 +1,7 @@
 {
 	"Kingdom Sans ms_0.ttf": null,
 	"PowerClear.ttf": null,
-	"README.md": 2535287125,
+	"README.md": 1362568535,
 	"RussellSquare.ttf": 1572031365,
 	"RussellSquareth.ttf": 1798953345,
 	"global.js": 327920362,
