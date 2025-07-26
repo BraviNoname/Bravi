76b6d5bbebfde73127c085d3b55431d21ad990bf
BraviNoname <braviaggron@gmail.com>
2025-07-27 01:21:20 +0200

    transparency i guess

--- Letoile/files.js
+++ Letoile/files.js
@@ -1,7 +1,7 @@
 {
 	"RussellSquare.ttf": null,
 	"RussellSquareth.ttf": 1798953345,
-	"Sereinae Létoile Omake.txt": 2656213389,
+	"Sereinae Létoile Omake.txt": 3877860454,
 	"Some Sereinae Sanae and Jolty dialogue.txt": 2286067871,
 	"global.js": 1012714159,
 	"patch.js": 1634924582,
