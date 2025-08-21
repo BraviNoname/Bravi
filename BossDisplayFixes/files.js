508e37b942cb0afa8209fd7a91963048b7545072
BraviNoname <braviaggron@gmail.com>
2025-08-21 21:26:50 +0200

    thanks neo nickz for the help!

--- BossDisplayFixes/files.js
+++ BossDisplayFixes/files.js
@@ -806,6 +806,7 @@
 	"th20/ascii/ascii.png": 520426434,
 	"th20/ascii/ascii_1280.png": 3546746933,
 	"th20/ascii/ascii_960.png": 2231270556,
+	"th20/default.ecl": 4136348473,
 	"th20/front/ename.png": 1187934677,
 	"th20/front/front00.png": 2082469526,
 	"th20/st03bs.ecl": 3310809427,
