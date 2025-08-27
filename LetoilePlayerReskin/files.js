c5499f58259979f2d587674ad479964afbf7a202
BraviNoname <braviaggron@gmail.com>
2025-08-27 04:40:30 +0200

    ok

--- LetoilePlayerReskin/files.js
+++ LetoilePlayerReskin/files.js
@@ -1,6 +1,6 @@
 {
 	"RussellSquare.ttf": null,
-	"Sereinae incidents thoughts.txt": 3019386257,
+	"Sereinae incidents thoughts.txt": 2160054369,
 	"global.js": 1522336802,
 	"patch.js": 2968946180,
 	"stringdefs.js": null,
