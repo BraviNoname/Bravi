dfd8857e160c87a4fa48d6886b18acaac005bd50
BraviNoname <braviaggron@gmail.com>
2025-08-31 17:17:38 +0200

    rewrote this, a bit

--- Letoile/files.js
+++ Letoile/files.js
@@ -1,7 +1,7 @@
 {
 	"RussellSquare.ttf": null,
 	"RussellSquareth.ttf": 1798953345,
-	"Sereinae Létoile Omake.txt": 3703960541,
+	"Sereinae Létoile Omake.txt": 3304720795,
 	"Some Sereinae Sanae and Jolty dialogue.txt": 2286067871,
 	"global.js": 1012714159,
 	"patch.js": 1634924582,
