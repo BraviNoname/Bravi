32d12f742df88018ce22db6c431fc073809881c7
BraviNoname <braviaggron@gmail.com>
2025-10-18 19:54:10 +0200

    thanks neo

---
+++ HomingSeija/th143.v1.00a.js
@@ -0,0 +1,14 @@
+{
+    "options": {
+        "nametest": {
+            "type": "s8",
+            "val": "scoreth143_Homing.dat"
+        }
+    },
+	"binhacks": {
+        "change_scorefile1": {
+            "addr": ["0x455e61", "0x4564d6"],
+            "code": "B9 <option:nametest>"
+        },
+	}
+}
