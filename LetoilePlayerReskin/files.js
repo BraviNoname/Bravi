570d581a10b3c3120a7e927c8a96081b31ac4912
BraviNoname <braviaggron@gmail.com>
2025-12-20 10:12:02 +0100

    reorganized stuff

--- LetoilePlayerReskin/files.js
+++ LetoilePlayerReskin/files.js
@@ -1,6 +1,6 @@
 {
 	"RussellSquare.ttf": null,
-	"Sereinae incidents thoughts.txt": 1430431908,
+	"Sereinae incidents thoughts.txt": 3955843941,
 	"global.js": 1522336802,
 	"patch.js": 21970092,
 	"stringdefs.js": null,
