997af75fab9cf64cbd36d3c7655d6b17b8d9d33d
BraviNoname <braviaggron@gmail.com>
2025-12-31 07:05:53 +0100

    adding thing

--- LetoilePlayerReskin/files.js
+++ LetoilePlayerReskin/files.js
@@ -1,6 +1,6 @@
 {
 	"RussellSquare.ttf": null,
-	"Sereinae incidents thoughts.txt": 3590046110,
+	"Sereinae incidents thoughts.txt": 1135283817,
 	"global.js": 1522336802,
 	"patch.js": 21970092,
 	"stringdefs.js": null,
