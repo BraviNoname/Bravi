1265cfd90df1deec63f6eef2af8f5b6e91a8b0a5
BraviNoname <braviaggron@gmail.com>
2026-01-05 18:11:37 +0100

    new mod

---
+++ PIERRE/files.js
@@ -0,0 +1,9 @@
+{
+	"RussellSquareth.ttf": 1798953345,
+	"global.js": 327920362,
+	"patch.js": 1048274137,
+	"stringdefs.js": null,
+	"th143.v1.00a.js": null,
+	"th143/pl00a.sht": null,
+	"th16/pl03.sht": 1648241302
+}
---
+++ PIERRE/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	]
+	"id": "PIERRE",
+	"servers": [
+		"https://raw.githubusercontent.com/BraviNoname/Bravi/master/PIERRE/"
+	],
+	"title": "(Th20) Replaces Ariya's face with Brock's from the Pokémon anime, referencing a french Youtube Poop from SuperRosel."
+}
