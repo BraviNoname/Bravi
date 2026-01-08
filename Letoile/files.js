0fd74f68361375fd8e298d3d61ba9b610d9917de
BraviNoname <braviaggron@gmail.com>
2026-01-08 19:54:59 +0100

    trying to polish some parts

--- Letoile/files.js
+++ Letoile/files.js
@@ -2,7 +2,7 @@
 	"RussellSquare.ttf": null,
 	"RussellSquareth.ttf": 1798953345,
 	"Sereinae Létoile Omake.txt": 1469531166,
-	"Sereinae Létoile Story Arcs.txt": 3783780233,
+	"Sereinae Létoile Story Arcs.txt": 3355871119,
 	"Some Sereinae Sanae and Jolty dialogue.txt": 2286067871,
 	"global.js": 1012714159,
 	"patch.js": 1634924582,
