ddc2a123abe1675078f98fcc69e43f4a6cb82f2f
BraviNoname <braviaggron@gmail.com>
2026-01-11 22:12:22 +0100

    more pierre

--- BossDisplayFixes/files.js
+++ BossDisplayFixes/files.js
@@ -1,7 +1,7 @@
 {
 	"Kingdom Sans ms_0.ttf": null,
 	"PowerClear.ttf": null,
-	"README.md": 4259158010,
+	"README.md": 1304174455,
 	"RussellSquare.ttf": 1572031365,
 	"RussellSquareth.ttf": 1798953345,
 	"global.js": 327920362,
--- PIERRE/files.js
+++ PIERRE/files.js
@@ -1,6 +1,14 @@
 {
 	"RussellSquareth.ttf": 1798953345,
 	"patch.js": 3503004281,
+	"th20/face/enemy6/face01an.png": 3178251545,
 	"th20/face/enemy6/face01ct.png": 3634772650,
-	"th20/face/enemy6/face01n2.png": 231645233
+	"th20/face/enemy6/face01dp.png": 1190089119,
+	"th20/face/enemy6/face01hp.png": 3471272184,
+	"th20/face/enemy6/face01n2.png": 231645233,
+	"th20/face/enemy6/face01no.png": 50774724,
+	"th20/face/enemy6/face01pr.png": 3299435104,
+	"th20/face/enemy6/face01sp.png": 1491211087,
+	"th20/face/enemy6/face01sw.png": 3956664767,
+	"th20/stgenm/stage06/enm6.png": 3183520876
 }
