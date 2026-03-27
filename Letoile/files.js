0920256b594d3f9ff57f46ff1817bc9727a53ba4
BraviNoname <braviaggron@gmail.com>
2026-03-27 01:23:02 +0100

    title change

--- Letoile/files.js
+++ Letoile/files.js
@@ -1,7 +1,7 @@
 {
 	"RussellSquare.ttf": null,
 	"RussellSquareth.ttf": 1798953345,
-	"Sereinae Létoile Omake.txt": 1793524192,
+	"Sereinae Létoile Omake.txt": 3753804170,
 	"Sereinae Létoile Story Arcs.txt": 1303983563,
 	"Some Sereinae Sanae and Jolty dialogue.txt": 2286067871,
 	"global.js": 1012714159,
